19fc39e5b7222d1a4b0aacf2509511bbf09f026e
Utkarsh Shah <utkarshshah007@gmail.com>
2014-12-03 02:28:50 -0500

    Movies search from prior to their release date
    Dates Fixed in movies file

--- TopsySearch.py
+++ TopsySearch.py
@@ -1,11 +1,14 @@
 # scrape tweets from topsy.com
  
-import sys, urllib, urllib2, json, random
+import sys, urllib, urllib2, json, random, locale
 from scrapeBoxOfficeMojo import getMovieList
+from datetime import datetime
 
  
-def search(query, page=1, perpage=100):
-  data = {'q': query, 'type': 'tweet', 'page': page, 'perpage': perpage, 'window': 'a', 'sort_method': "-date", 'apikey': '09C43A9B270A470B8EB8F2946A9369F3', "mintime":1414792811, "maxtime":1417125633}
+def search(query, page=1, perpage=100, maxtime=None):
+  data = {'q': query, 'type': 'tweet', 'page': page, 'perpage': perpage, 'sort_method': "-date", 'apikey': '09C43A9B270A470B8EB8F2946A9369F3', "maxtime":maxtime}
+  if maxtime is None:
+    data = {'q': query, 'type': 'tweet', 'page': page, 'perpage': perpage, 'sort_method': "-date", 'apikey': '09C43A9B270A470B8EB8F2946A9369F3'}
   url = "http://otter.topsy.com/search.js?" + urllib.urlencode(data)
   data = urllib2.urlopen(url)
   o = json.loads(data.read())
@@ -17,15 +20,14 @@ def get_movies_and_dates():
   f = open("movies.txt", "r")
   movieAndDateList = [movie.strip('\n') for movie in f]
   dateList = [movie.split("|")[0] for movie in movieAndDateList]
+  locale.setlocale(locale.LC_ALL, '')
+  dateList = [datetime.strptime(date,"%x") for date in dateList]
   return zip(movieList, dateList);
 
-def string_to_date(dateString):
-  pass
-
-def get_all_tweets(movie):
+def get_all_tweets(movie, releaseDate):
   all_tweets = []
   for page in range(1,11):
-    res = search(movie, page=page)
+    res = search(movie, page=page, maxtime=releaseDate)
     tweets = [{"text":tweet['title'], "date":tweet['firstpost_date']} for tweet in res['list']]
     all_tweets.extend(tweets)
   return all_tweets
@@ -34,14 +36,13 @@ def dump_to_file(obj, outfile):
   o = open(outfile, "w")
   o.write(json.dumps(obj))
   o.close()
-
  
 if __name__ == "__main__":
   moviesAndDates = get_movies_and_dates()
   all_movie_data = []
   for movie,date in movieList:
     print movie
-    tweets = get_all_tweets(movie)
+    tweets = get_all_tweets(movie, date)
     all_movie_data.extend([{movie:tweet} for tweet in tweets])
 
   dump_to_file(all_movie_data, "movie_tweet_data")
