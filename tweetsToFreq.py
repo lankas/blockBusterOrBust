8b4ab21303f27b3e6f46aacb37728f9b31a04221
Sarah <xsarah.dean@gmail.com>
2014-12-02 23:39:57 -0500

    edits to freq file

--- tweetsToFreq.py
+++ tweetsToFreq.py
@@ -7,7 +7,20 @@
 """
 
 import numpy as np
+import json
+
+#each movie has a json - dictionary is date and text
+
+#these things will  be arguments potentially
+releaseTime
+tweetsFile
 
 
+with open(tweetsFile) as json_data:
+    tweets = json.loads(json_data)
+    pprint(d)
+
+for tweet in tweets
+    tweet.time #unix  
 
 np.savetxt("freqMovies.data", freq_vect, delimiter=",")
