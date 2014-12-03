f18a02cf5858f5c197039aff455e7498ddf59a31
Sarah <xsarah.dean@gmail.com>
2014-12-02 22:27:07 -0500

    first draft prediction and created frequency

---
+++ tweetsToFreq.py
@@ -0,0 +1,13 @@
+"""
+
+ author: Sarah
+ script take a collection of tweets for a movie and transform it into a frequency vector
+ still to-do:
+
+"""
+
+import numpy as np
+
+
+
+np.savetxt("freqMovies.data", freq_vect, delimiter=",")
